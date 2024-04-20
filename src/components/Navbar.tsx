8c89b85aba1f18e2cb23bc961126b594f85805d6
soohyunnn <sksmsqodn20@gmail.com>
2024-04-20 13:47:31 +0900

    Design : 모바일 환경에서 햄버거 메뉴바 UI 구현

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,72 @@ import styled from "styled-components";
 export default function Navbar() {
   const pathName = usePathname();
 
+  const toggleSidebar = () => {
+    console.log("toggleSidebar");
+
+    // const menu = document.querySelector(".menu");
+    document
+      .getElementsByClassName("section_bar")[0]
+      .classList.toggle("active");
+    document
+      .getElementsByClassName("navbar_container")[0]
+      .classList.toggle("active");
+  };
+
   return (
-    <div className="navbar_container">
-      <Link href="/" style={{ textDecoration: "none" }}>
-        <img src="/stayinterviewLogo.png" alt="사이트 로고" />
-      </Link>
-      <MiddleNavbarDiv className="navbar_div" style={{ marginLeft: "15px" }}>
+    <>
+      <div className="navbar_container">
+        <Link href="/" style={{ textDecoration: "none" }}>
+          <img src="/stayinterviewLogo.png" alt="사이트 로고" />
+        </Link>
+        <MiddleNavbarDiv className="navbar_div" style={{ marginLeft: "15px" }}>
+          <Link
+            href={{ pathname: "/ourStay" }}
+            style={{ textDecoration: "none" }}
+          >
+            <MenuDiv>OUR STAY</MenuDiv>
+          </Link>
+          <Link
+            href={{ pathname: "/consulting" }}
+            style={{ textDecoration: "none" }}
+          >
+            <MenuDiv>CONSULTING</MenuDiv>
+          </Link>
+          <Link
+            href={{ pathname: "/ourNews" }}
+            style={{ textDecoration: "none" }}
+          >
+            <MenuDiv>OUR NEWS</MenuDiv>
+          </Link>
+        </MiddleNavbarDiv>
+        <RightNavbarDiv className="navbar_div">
+          <Link
+            href={{ pathname: "/login" }}
+            style={{ textDecoration: "none" }}
+          >
+            <MenuDiv>Login</MenuDiv>
+          </Link>
+          <Link
+            href="https://www.instagram.com/stay.interview"
+            style={{ width: "36px", height: "36px" }}
+          >
+            <Image
+              src="/instagramLogo.png"
+              alt="인스타그램 로고"
+              width={36}
+              height={36}
+            />
+          </Link>
+        </RightNavbarDiv>
+        <div className="menu-bar">
+          <div className="menu menu-icon" onClick={toggleSidebar}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
+          </div>
+        </div>
+      </div>
+      <div className="section_bar">
         <Link
           href={{ pathname: "/ourStay" }}
           style={{ textDecoration: "none" }}
@@ -32,24 +92,34 @@ export default function Navbar() {
         >
           <MenuDiv>OUR NEWS</MenuDiv>
         </Link>
-      </MiddleNavbarDiv>
-      <RightNavbarDiv className="navbar_div">
+
         <Link href={{ pathname: "/login" }} style={{ textDecoration: "none" }}>
-          <MenuDiv>Login</MenuDiv>
+          <MenuDiv style={{ marginTop: "20px" }}>Login</MenuDiv>
         </Link>
-        <Link
-          href="https://www.instagram.com/stay.interview"
-          style={{ width: "36px", height: "36px" }}
-        >
-          <Image
-            src="/instagramLogo.png"
-            alt="인스타그램 로고"
-            width={36}
-            height={36}
-          />
-        </Link>
-      </RightNavbarDiv>
-    </div>
+
+        <div style={{ marginTop: "40px" }}>
+          <div>
+            <MenuDiv>contact us</MenuDiv>
+            <MenuDiv style={{ fontSize: "15px" }}>
+              stay_interview@naver.com
+            </MenuDiv>
+          </div>
+
+          <Link
+            href="https://www.instagram.com/stay.interview"
+            style={{ width: "36px", height: "36px" }}
+          >
+            <Image
+              src="/instagramLogo.png"
+              alt="인스타그램 로고"
+              style={{ margin: "20px 0" }}
+              width={36}
+              height={36}
+            />
+          </Link>
+        </div>
+      </div>
+    </>
   );
 }
 
